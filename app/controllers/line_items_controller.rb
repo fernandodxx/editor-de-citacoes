edb2c1c3e8680058dbdc7dac6082995becf896cd
fernandodxx <fernandosillvasantoss@gmail.com>
2025-10-16 10:20:26 -0300

    Add edit update

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,6 +1,7 @@
 class LineItemsController < ApplicationController
   before_action :set_quote
   before_action :set_line_item_date
+  before_action :set_line_item, only: [ :edit, :update, :destroy ]
 
   def new
     @line_item = @line_item_date.line_items.build
@@ -16,10 +17,25 @@ class LineItemsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @line_item.update(line_item_params)
+      redirect_to quote_path(@quote), notice: "Item was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def line_item_params
     params.require(:line_item).permit(:name, :description, :quantity, :unit_price)
+  end
+
+  def set_line_item
+    @line_item = @line_item_date.line_items.find(params[:id])
   end
 
   def set_quote
