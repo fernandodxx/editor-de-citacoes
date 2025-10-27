b0ef9a9be92d3abd52226ababdb3d284acc439aa
fernandodxx <fernandosillvasantoss@gmail.com>
2025-10-27 09:05:38 -0300

    Editing line dates with turbo

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -27,7 +27,7 @@ class LineItemsController < ApplicationController
     if @line_item.update(line_item_params)
       respond_to do |format|
         format.html { edirect_to quote_path(@quote), notice: "Item was successfully updated." }
-        format.turbo_stream { flash.now[:notice] = "Item was successfully updated. "}
+        format.turbo_stream { flash.now[:notice] = "Item was successfully updated. " }
       end
     else
       render :edit, status: :unprocessable_entity
@@ -39,7 +39,7 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       format.html { redirect_to quote_path(@quote), notice: "Date was successfully destroyed." }
-      format.turbo_stream { flash.now[:notice] = "Date was successfully destroyed."}
+      format.turbo_stream { flash.now[:notice] = "Date was successfully destroyed." }
     end
   end
 
